2e692af04d1a7fabe4516c951a4c01b0d6e70c5f
Obed Rayo <obedrayo2356@gmail.com>
2023-05-28 20:56:05 -0500

    Adding principal page

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion"
 
 const MotionLink = motion(Link);
 
@@ -17,6 +17,7 @@ const Logo = () => {
                     repeat: Infinity
                 },
             }}>
+
                 OR
             </MotionLink>
 
--- src/components/icons.js
+++ src/components/icons.js
@@ -447,7 +447,7 @@ export const LinkArrow = ({ className, ...rest }) => (
     width={24}
     height={24}
     viewBox="0 0 24 24"
-    className={`w-full h-auto ${className}`}
+    className={`inline ${className}`}
     {...rest}
   >
     <path fill="none" d="M0 0h24v24H0z" />
