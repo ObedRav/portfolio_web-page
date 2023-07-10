2cbf281392f72b4f4a1afc552af290918851b167
Obed Rayo <obedrayo2356@gmail.com>
2023-07-10 17:53:37 -0500

    Adding About Page (#5)
    * Adding description
    
    * Developing about page
    
    * Updating dependencies (#4)
    
    * fix: upgrade eslint from 8.36.0 to 8.43.0 (#3)
    
    Snyk has created this PR to upgrade eslint from 8.36.0 to 8.43.0.
    
    See this package in npm:
    https://www.npmjs.com/package/eslint
    
    See this project in Snyk:
    https://app.snyk.io/org/obedrav/project/875b8c9c-ea1d-403d-9649-42f0c746c722?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    
    * fix: upgrade next from 13.2.4 to 13.4.6 (#2)
    
    Snyk has created this PR to upgrade next from 13.2.4 to 13.4.6.
    
    See this package in npm:
    https://www.npmjs.com/package/next
    
    See this project in Snyk:
    https://app.snyk.io/org/obedrav/project/875b8c9c-ea1d-403d-9649-42f0c746c722?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    
    * fix: upgrade eslint-config-next from 13.2.4 to 13.4.6 (#1)
    
    Snyk has created this PR to upgrade eslint-config-next from 13.2.4 to 13.4.6.
    
    See this package in npm:
    https://www.npmjs.com/package/eslint-config-next
    
    See this project in Snyk:
    https://app.snyk.io/org/obedrav/project/875b8c9c-ea1d-403d-9649-42f0c746c722?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    
    ---------
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    
    * Adding principal components for the about page
    
    ---------
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>

---
+++ src/pages/about.js
@@ -0,0 +1,109 @@
+/* eslint-disable react/no-unescaped-entities */
+import AnimatedText from '@/components/AnimatedText'
+import Layout from '@/components/Layout'
+import Head from 'next/head'
+import Image from 'next/image'
+import React, { useEffect, useRef } from 'react'
+import profilePicture from '../../public/images/profile_picture.png'
+import { useInView, useMotionValue, useSpring } from 'framer-motion'
+
+const AnimatedNumbers = ({value}) => {
+    const ref = useRef(null);
+
+    const motionValue = useMotionValue(0);
+    const springValue = useSpring(motionValue, { duration: 3000 });
+    const isInView = useInView(ref, { once: true });
+
+    useEffect(() => {
+        if (isInView) {
+            motionValue.set(value)
+        }
+    }, [isInView, value, motionValue]);
+
+    useEffect(() => {
+        springValue.on('change', (latest) => {
+            if (ref.current && latest.toFixed(0) <= value) {
+                ref.current.textContent = latest.toFixed(0);
+            }
+        })
+    }, [springValue, value])
+
+    return <span ref={ref}></span>
+}
+
+
+const about = () => {
+  return (
+    <>
+        <Head>
+            <title>ObedRav | About</title>
+            <meta name='description' content='about devloper page' />
+        </Head>
+        <main className='flex w-full flex-col items-center justify-center'>
+            <Layout className='pt-16'>
+                <AnimatedText text='Code Crafted with Heart and Mind!' className='text-7xl mb-16' />
+                <div className='grid w-full grid-cols-8 gap-16'>
+
+                    {/** About me and text */}
+                    <div className='col-span-3 flex flex-col items-start justify-start'>
+                        <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'>About me</h2>
+
+                        <p  className='font-medium'>
+                            Hi, I'm <b>Obed Rayo</b>, a passionate software developer with expertise in full-stack
+                            web development and a specialization in backend technologies. With experience in
+                            the field, I have honed my skills in creating robust and scalable solutions.
+                        </p>
+
+                        <p className='my-4'>
+                            When it comes to backend development, I believe it goes beyond building fast and
+                            functional systems. It's about solving problems and making a positive impact on
+                            people's lives. I view backend development as a means to create innovative and
+                            user-centric experiences that address real-world challenges.
+                        </p>
+
+                        <p className='my-4'>
+                            I thrive on challenges and constantly seek opportunities to push the boundaries of
+                            what's possible in backend development. I'm always eager to explore new technologies,
+                            frameworks, and methodologies to enhance my skills and deliver even better solutions.
+                        </p>
+                    </div>
+
+                    {/** PROFILE PICTURE */}
+                    <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8'>
+                        <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark' />
+                        <Image src={profilePicture} alt='profile picture' className='w-full h-auto rounded-2xl' />
+                    </div>
+
+                    {/** Graph */}
+                    <div className='col-span-2 flex flex-col items-end justify-between'>
+
+                        <div className='flex flex-col items-end justify-center'>
+                            <span className='inline-block text-7xl font-bold'>
+                                <AnimatedNumbers value={3000} />+
+                            </span>
+                            <h2 className='text-xl font-medium capitalize text-dark/75'>Development Hours</h2>
+                        </div>
+
+                        <div className='flex flex-col items-end justify-center'>
+                            <span className='inline-block text-7xl font-bold'>
+                                <AnimatedNumbers value={70} />+
+                            </span>
+                            <h2 className='text-xl font-medium capitalize text-dark/75'>Projects Completed</h2>
+                        </div>
+
+                        <div className='flex flex-col items-end justify-center'>
+                            <span className='inline-block text-7xl font-bold'>
+                                <AnimatedNumbers value={1} />+
+                            </span>
+                            <h2 className='text-xl font-medium capitalize text-dark/75'>Years of Experience</h2>
+                        </div>
+
+                    </div>
+                </div>
+            </Layout>
+        </main>
+    </>
+  )
+}
+
+export default about
