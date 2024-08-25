e4a59262130f8fe9bdabf995f056eb55a14a575b
obedrayo <milton.rayo@soytul.com>
2024-08-25 13:39:56 -0500

    Updating cache control to catch

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  headers() {
+    return [
+      {
+        source: '/images/:all*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=2592000, stale-while-revalidate=86400, stale-if-error=2592000',
+          },
+        ]
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;
