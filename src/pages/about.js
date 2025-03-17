483a66f1e4184b4868f3a3e7b8b6a743d76bee97
Obed Rayo <obedrayo2356@gmail.com>
2025-03-16 20:09:51 -0500

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -69,7 +69,7 @@ const about = () => {
 
               <div className='flex flex-col items-end justify-center xl:items-center '>
                 <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
-                  <AnimatedNumbers value={2} />+
+                  <AnimatedNumbers value={3} />+
                 </span>
                 <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>{t('about.numbersText3')}</h2>
               </div>
